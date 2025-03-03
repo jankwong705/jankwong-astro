346905fa945ce6cc97ba9d6bcfc4fbf7a4798a57
jankwong705 <114449531+jankwong705@users.noreply.github.com>
2025-03-02 19:48:44 -0800

    try

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  outDir: 'dist',
+  build: {
+    format: 'directory' // Ensures /about/ becomes /about/index.html instead of about.html
+  }
+});
